f12948ff22f104f2e5293f9f7fa611ae88d5c83d
csmoon1010 <hjmoon1010@gmail.com>
2021-04-11 12:29:08 +0900

    Spring core

---
+++ diexam01/src/main/java/kr/or/connect/diexam01/ApplicationConfig2.java
@@ -0,0 +1,10 @@
+package kr.or.connect.diexam01;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("kr.or.connect.diexam01") //사용자가 알려주지 않아도 알아서 찾아서 등록하기!! - 스캔할 패키지 범위 설정
+public class ApplicationConfig2 {
+
+}
